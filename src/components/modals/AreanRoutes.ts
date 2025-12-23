f34890b479a451a9be74e58ea3e03a9f4d58c95b
Masud Al Imran <masudalimran92@gmail.com>
2025-12-23 18:16:13 +0600

    added doom tower boss pages

--- src/components/modals/AreanRoutes.ts
+++ src/components/modals/AreanRoutes.ts
@@ -2,6 +2,7 @@ import { buildAreaRoutes } from "../../helpers/buildAreaRoutes";
 import { ChampionFaction } from "../../models/ChampionFaction";
 import { ARENA } from "../../models/game_areas/Arena";
 import { CLAN_BOSS } from "../../models/game_areas/ClanBoss";
+import { DOOM_TOWER_BOSS } from "../../models/game_areas/DoomTowerBoss";
 import { DUNGEON } from "../../models/game_areas/Dungeon";
 import { HYDRA } from "../../models/game_areas/Hydra";
 import { POTION_KEEP } from "../../models/game_areas/PotionKeep";
@@ -30,5 +31,9 @@ export const AREA_ROUTES = [
   ...buildAreaRoutes(ChampionFaction, {
     maxChampions: 5,
     isFaction: true,
+  }),
+
+  ...buildAreaRoutes(DOOM_TOWER_BOSS, {
+    maxChampions: 5,
   }),
 ] as const;
