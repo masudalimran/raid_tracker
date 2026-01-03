a178365147cef862bf79bd4341712cb990f03ec9
Masud Al Imran <masudalimran92@gmail.com>
2026-01-03 16:15:01 +0600

    Update

--- src/components/forms/inputs/ChampionMultiSelect.tsx
+++ src/components/forms/inputs/ChampionMultiSelect.tsx
@@ -1,6 +1,8 @@
 import { Fragment, useState } from "react";
 import type IChampion from "../../../models/IChampion";
 import colorByRarity from "../../../helpers/colorByRarity";
+import { HiOutlineExternalLink } from "react-icons/hi";
+import { checkIfChampionIsBuilt } from "../../../helpers/checkIfChampionIsBuilt";
 
 interface ChampionMultiSelectProps {
   value: string[];
@@ -47,20 +49,46 @@ export default function ChampionMultiSelect({
           .map((champ) =>
             champ.id ? (
               <Fragment key={champ.id}>
-                <label
-                  className={`flex items-center gap-2 cursor-pointer basic-padding-xs ${colorByRarity(
-                    champ.rarity
-                  )}`}
-                >
-                  <input
-                    type="checkbox"
-                    checked={value.includes(champ.id.toString())}
-                    onChange={() =>
-                      champ.id && toggleChampion(champ.id.toString())
-                    }
-                  />
-                  {champ.name}
-                </label>
+                <div className="flex items-center gap-1 w-full pr-4">
+                  <label
+                    className={`flex-1 flex items-center gap-2 cursor-pointer basic-padding-xs ${colorByRarity(
+                      champ.rarity
+                    )}`}
+                  >
+                    <input
+                      type="checkbox"
+                      checked={value.includes(champ.id.toString())}
+                      onChange={() =>
+                        champ.id && toggleChampion(champ.id.toString())
+                      }
+                    />
+                    <div>
+                      <img
+                        src={champ.imgUrl}
+                        className="h-5 w-5 object-cover rounded-full"
+                      />
+                    </div>
+                    {champ.name}
+                    <div
+                      className={`w-5 h-5 flex-center text-xs text-white rounded-full ${
+                        checkIfChampionIsBuilt(champ)
+                          ? "bg-green-500"
+                          : "bg-red-500"
+                      }`}
+                    >
+                      B
+                    </div>
+                  </label>
+                  <a
+                    href={champ.championUrl}
+                    target="_blank"
+                    className={`${colorByRarity(
+                      champ.rarity
+                    )} h-8 w-8 flex-center hover:opacity-75 transition`}
+                  >
+                    <HiOutlineExternalLink />
+                  </a>
+                </div>
               </Fragment>
             ) : (
               <p>Champion Id Does Not Exist!!</p>
